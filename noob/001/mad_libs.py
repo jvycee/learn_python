cf5a65dda8b3d93a27589032ed32beec4c77c979
jvycee <jvycee@pm.me>
2024-12-18 23:13:17 -0700

    mad libs game

---
+++ noob/001/mad_libs.py
@@ -0,0 +1,8 @@
+name = input("Name: ")
+adj = input("Adjective: ")
+verb = input("Verb: ")
+mineral = input("Mineral: ")
+metal = input("Metal: ")
+food = input("Food: ")
+
+print(f"{name} is a {adj} programmer who loves to {verb} {mineral}. And they like to eat {metal} {food}.")
