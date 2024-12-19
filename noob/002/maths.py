848d34dc2b17f7c8873875780d1e629169bc2f5a
jvycee <jvycee@pm.me>
2024-12-18 23:38:40 -0700

    added some maths

--- noob/002/maths.py
+++ noob/002/maths.py
@@ -1,1 +1,18 @@
 # This is to learn about types of maths available in python
+
+friends = 0
+# # add
+# # friends += 1
+# # sub
+# friends -= 1
+# # mul
+# friends *= 2
+# # div
+# friends /= 2
+# exp
+friends **= 2
+# mod
+friends %= 2
+
+
+print(friends)
