8a5e5e1827b6b8f6eb46a001bdd7e63f797635f7
jvycee <jvycee@pm.me>
2024-12-19 21:33:32 -0700

    if statements galore

---
+++ noob/001/calc.py
@@ -0,0 +1,20 @@
+# a Python calculator
+
+operator = input("Enter an operator (+ - * /) ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+     result = num1 - num2
+     print(result)
+elif operator == "*":
+     result = num1 * num2
+     print(result)
+elif operator == "/":
+     result = num1 / num2
+     print(result)
+
---
+++ noob/001/if_statements.py
@@ -0,0 +1,36 @@
+# age = int(input("Enter your age: "))
+
+# if age >= 18:
+#     print("You are now signed up")
+# elif age < 0:
+#     print("Be serious")
+# else: 
+#     print("Just a baby")
+
+# response = input("Food? Y/N: ").lower()
+# yes_responses = ["y", "yes", "yep", "ok"]
+# if response in yes_responses:
+#     print("Eat up")
+# else:
+#     print("No food for you")
+
+# name = input("Enter your name: ")
+
+# if name == "":
+#     print("You dick. Enter your name. ")
+# else:
+#     print(f"Hello {name}")
+    
+    
+# for_sale = False
+# if for_sale:
+#     print("Buy it")
+# else: 
+#     print("Nope")\
+    
+virgin = True
+
+if virgin:
+    print("Hand doesn't count")
+else:
+    print("Keep trying")
