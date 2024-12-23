2674518145581327882202e7e68e4def746f5692
jvycee <jvycee@pm.me>
2024-12-22 20:35:05 -0700

    int and float

---
+++ corey_s_python/beginners/integers_and_floats/001.py
@@ -0,0 +1,9 @@
+# num = 3.14
+# print(type(num))
+
+num_1 = 1
+num_2 = 5
+num_3 = "3"
+num_4 = "5"
+# print(num_1 == num_2)
+print(int(num_3) + int(num_4))
--- corey_s_python/beginners/textural_data/001.py
+++ corey_s_python/beginners/textural_data/001.py
@@ -1,5 +1,25 @@
-# Print something
-message = """Hello, world! I like
-coding and cats. """
-print(message)
+# # Print something
+# message = """Hello, world! I like
+# coding and cats. """
+# print(message[6:])
 
+# string method
+
+# message = "Hello, world! "
+# # print(message.lower())
+# # print(message.upper())
+# print(message.find('world'))
+
+# message = "Hello, world! "
+# # print(message.lower())
+# # print(message.upper())
+# message = message.replace('world', 'fuckers')
+
+# print(message)
+
+greeting = 'Hello'
+name = 'Jaycee'
+
+# message = greeting + ', ' + name
+message = f'{greeting}, {name}. Welcome!'
+print(message)
