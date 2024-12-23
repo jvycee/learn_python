accdd3811340ccbeabb1cb4a79491f651cbc986e
jvycee <jvycee@pm.me>
2024-12-22 22:22:55 -0700

    list, tuple, set

---
+++ corey_s_python/beginners/list_tuples_sets/001.py
@@ -0,0 +1,41 @@
+# fun with Lists
+
+courses = ['history', 'maths', 'knitting', 'python']
+# courses_2 = ['PE', 'philosophy']
+# nums = [2, 3, 6, 1, 8]
+# print(len(courses))
+# print(courses[0:2])
+# courses.extend(courses_2)
+# print(courses[0])
+# print(courses_2)
+# courses.pop()
+# courses.sort(reverse=True)
+# sorted_courses = sorted(courses)
+# print(sorted_courses)
+
+# print(sum(nums))
+
+# print(courses.index('Art'))
+# print('Art' in courses)
+# for index, item in enumerate(courses):
+#     print(index, item)
+# course_str = ', '.join(courses)
+# print(course_str)
+
+# more fun
+
+# Sets
+# cs_courses = {'HTML', 'JS', 'Ruby', 'Python'}
+
+# print('Math' in cs_courses)
+
+# Create empty Lists, Tuples, and Sets
+empty_lists = []
+empty_list = list()
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+empty_set = {}
+empty_set = set()
+
